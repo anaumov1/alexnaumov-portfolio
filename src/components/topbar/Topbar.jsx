0295a99b548f68d3186bc64ead09637dc5dac98a
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-09-19 18:03:32 -0500

    created components

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./topbar.scss";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">asdasd</a>
+          <h1>Hello</h1>
+        </div>
+        <div className="right">TESTING</div>
+      </div>
+    </div>
+  );
+}
