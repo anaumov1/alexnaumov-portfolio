a194847aac3a837e73a72294682760dce1224c5f
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-09-20 12:08:21 -0500

    top bar scss

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./topbar.scss";
+import { AccountCircle, MailOutline } from "@mui/icons-material";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">asdasd</a>
-          <h1>Hello</h1>
+          <a href="#intro" className="logo">
+            Coffee and Code
+          </a>
+          <div className="itemContainer">
+            <AccountCircle className="icon" />
+            <span>+1-512-574-5105</span>
+          </div>
+          <div className="itemContainer">
+            <MailOutline className="icon" />
+            <span>alexandru.naumov@gmail.com</span>
+          </div>
         </div>
-        <div className="right">TESTING</div>
+        <div className="right" onClick={() => setMenuOpen(!menuOpen)}>
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
