5a645efdaac021ab28891e88ff8f89be53be59a0
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-09-22 08:54:26 -0500

    Intro section desigb completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ["React", "JavaScript", "NodeJS", "UX/UI Design", "MERN"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,7 @@ export default function Intro() {
           <h2>Hello, I'm</h2>
           <h1>Alex Naumov</h1>
           <h3>
-            Web Developer<span></span>
+            Web Developer <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="iem">
+          <img src="assets/myregistry.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
