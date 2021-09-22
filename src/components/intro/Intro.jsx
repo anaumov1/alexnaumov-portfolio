5b24d943a95aecd3bfea189beb0445cd6865864a
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-09-21 22:18:05 -0500

    added assets, started design for intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,5 +2,25 @@ import React from "react";
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/cutout.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello, I'm</h2>
+          <h1>Alex Naumov</h1>
+          <h3>
+            Web Developer<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
